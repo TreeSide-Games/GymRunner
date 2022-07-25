a74c414afa3a955605b7af57346f6ba028c83742
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-07-25 20:17:53 +0200

    Dodano generowanie przedmiotów na mapie

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] GameObject roadPrefab;
+    [SerializeField] List<GameObject> itemPrefab = new List<GameObject>();
 
     
-    [SerializeField] List<GameObject> roads = new List<GameObject>();
+    List<GameObject> roads = new List<GameObject>();
 
     int maxRoadToPlace = 10;
     int roadDistance = 0;
@@ -25,6 +26,7 @@ public class MapGenerator : MonoBehaviour
 
     private void AddRoad()
     {
+        ChooseItem();
         var road = Instantiate(roadPrefab);
 
         road.transform.position = Vector3.forward * roadDistance;
@@ -32,6 +34,39 @@ public class MapGenerator : MonoBehaviour
 
         roads.Add(road);
         roadDistance++;
+
+        PlaceItemOn(road.transform);
+    }
+
+    private GameObject ChooseItem()
+    {
+        int random = UnityEngine.Random.Range(0, System.Enum.GetValues(typeof(EnumItems)).Length);
+
+        EnumItems itemType = (EnumItems)random;
+
+        //return itemPrefab[0];
+        return itemPrefab.Find(e => e.GetComponent<Item>().itemType == itemType);
+        
+    }
+
+    private void PlaceItemOn(Transform road)
+    {
+        var itemToInstantiate = ChooseItem();
+        if (itemToInstantiate == null)
+            return;
+
+        var item = Instantiate(itemToInstantiate);
+        item.transform.parent = road.transform;
+
+        item.transform.localPosition = Vector3.forward + Vector3.up;
+        item.transform.localRotation = Quaternion.identity;
+
+        var randomnes = UnityEngine.Random.Range(0f,1f);
+
+        if (randomnes < 0.3f)
+            item.transform.localPosition += Vector3.right * 0.3f;
+        else if (randomnes > 0.6f)
+            item.transform.localPosition += Vector3.left * 0.3f;
     }
 
     IEnumerator GeneratorCoroutine()
@@ -54,4 +89,6 @@ public class MapGenerator : MonoBehaviour
         Destroy(roads[0]);
         roads.RemoveAt(0);
     }
+
+
 }
