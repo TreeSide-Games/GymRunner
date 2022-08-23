d44c0e726956c7881f784001e5b59f5078091b80
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-08-23 12:21:33 +0200

    Dodano losowy wybór piosenki

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,35 +8,56 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioClip collectingHealthFoodAudio;
     [SerializeField] AudioClip collectingFastFoodAudio;
     [SerializeField] AudioClip collectingCoinAudio;
-    [SerializeField] AudioClip mainMusic;
+    [SerializeField] List<AudioClip> mainMusic = new List<AudioClip>();
 
-    AudioSource audioSource;
+    AudioSource[] audioSource;
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponents<AudioSource>();
 
     }
     void Start()
     {
-        Item.OnCollectedItem += launchAudio;
+        Item.OnCollectedItem += launchCollectingAudio;
 
+        StartCoroutine(PlayMusicCoroutine());
     }
 
-    void launchAudio(Item item)
+    IEnumerator PlayMusicCoroutine()
+    {
+        float timeToEndMusic = 0;
+        while (true)
+        {
+            launchMusic();
+            timeToEndMusic = audioSource[1].clip.length;
+
+            yield return new WaitForSeconds(timeToEndMusic);
+        }
+    }
+
+    void launchCollectingAudio(Item item)
     {
         if (item.isMoney)
-            audioSource.clip = collectingCoinAudio;
+            audioSource[0].clip = collectingCoinAudio;
         else if(item.isHealthy)
-            audioSource.clip = collectingHealthFoodAudio;
+            audioSource[0].clip = collectingHealthFoodAudio;
         else
-            audioSource.clip = collectingFastFoodAudio;
+            audioSource[0].clip = collectingFastFoodAudio;
 
-        audioSource.Play();
+        audioSource[0].Play();
+    }
+
+    void launchMusic()
+    {
+        var randomnes = UnityEngine.Random.Range(0, mainMusic.Count);
+
+        audioSource[1].clip = mainMusic[randomnes];
+        audioSource[1].Play();
     }
 
     private void OnDisable()
     {
-        Item.OnCollectedItem -= launchAudio;
+        Item.OnCollectedItem -= launchCollectingAudio;
     }
 }
