7ef35b153d740b5f63c8577312497a285fd8560d
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-09-01 16:47:23 +0200

    Dodanie joysticka

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,8 +17,11 @@ public class PlayerMovement : MonoBehaviour
     #endregion
 
     public event Action OnSpeedChange;
-    [SerializeField] float speed = 2f;
-    
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Joystick joystick;
+    //private RectTransform joystickReckTransform;
+    //private Vector2 joystickNewPosition;
+
     public float Speed 
     {
         get { return speed; } 
@@ -31,18 +34,74 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        
+        //joystickReckTransform = joystick.GetComponent<RectTransform>();
     }
 
     
     void Update()
     {
-        transform.position += Vector3.forward * speed * Time.deltaTime;
 
-        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed / 100f;
+        //transform.position += Vector3.forward * speed * Time.deltaTime;
+        //transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed / 100f;
+
+
         //float horizontalMoveValue = Mathf.Clamp(1 * Input.mousePosition.normalized.x * speed * Time.deltaTime, -0.45f, 0.45f);
 
         //transform.position = new Vector3(horizontalMoveValue, transform.position.y);
 
+
+
+        //transform.position += Vector3.forward * speed * Time.deltaTime;
+        //float horizontalMoveValue2 = Input.mousePosition.normalized.x;
+        //transform.position += Vector3.right * horizontalMoveValue2 * speed / 100f;
+        //float move = Input.GetAxis("Horizontal");
+        //Debug.Log("Axis: " + move);
+        //Debug.Log("Mouse: " + horizontalMoveValue2);
+        
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        //if (joystickReckTransform.localScale.x == 0)
+        //{
+        //    if (Input.touchCount != 0)
+        //    {
+        //        Touch touch = Input.GetTouch(0);
+
+        //        RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickReckTransform, touch.position, null, out joystickNewPosition);
+
+        //        joystickReckTransform.localScale = Vector3.one;
+        //        joystickReckTransform.localPosition = joystickNewPosition;
+        //    }
+        //}
+        //else
+        //{
+        //    if (Input.touchCount == 0)
+        //    {
+        //        joystickReckTransform.localScale = Vector3.zero;
+        //        return;
+        //    }
+        //}
+
+        
+
+
+        //if(Input.touchCount > 0)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+        //    var touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        //    touchPosition.z = 0f;
+
+
+
+        //    joystick.gameObject.SetActive(true);
+        //    joystick.GetComponent<Transform>().localPosition = touchPosition;
+
+        //}
+        //else
+        //{
+        //    joystick.gameObject.SetActive(false);
+        //    return;
+        //}
+
+        transform.position += Vector3.right * joystick.Horizontal * speed * Time.deltaTime;
     }                                                                             
 }
